5b4507f4dfef8aa9ce5d2b6817defe6cd3ecbf59
Rajatkumar jena <rajat@Rajatkumars-MacBook-Air.local>
2024-12-25 21:02:56 -0800

    ui tech stack change with react

---
+++ Client/src/pages/home.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import Footer from "../components/footer";
+import NavBar from "../components/navBar";
+
+const Home = () => {
+  const [location, setLocation] = useState({});
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (location) {
+      console.log(location);
+    }
+  }, [location]);
+  return (
+    <>
+      <NavBar />
+
+      {/* body part  */}
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      {/* body part end  */}
+
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
